c72c1d9d31e75ab3a0bd37af952f399b4a421896
李昊 <1097261154@qq.com>
2023-06-18 11:12:15 +0800

    feat: CSS样式

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import App from './App.vue'
 import router from './router'
-
+import "./assets/reset.css"
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
